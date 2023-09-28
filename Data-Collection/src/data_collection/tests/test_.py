847a1fc5de509fb1ab95f8075730d907f0373a55
glass-ships <glass.ships@outlook.com>
2023-09-27 21:29:58 -0600

    add simple tests

--- Data-Collection/src/data_collection/tests/test_.py
+++ Data-Collection/src/data_collection/tests/test_.py
@@ -1,1 +1,16 @@
-import pytest 
+import requests
+
+import pandas as pd
+
+data_url = "https://services.swpc.noaa.gov/products/geospace/propagated-solar-wind-1-hour.json"
+local_data = "data/noaa_rswd.pkl"
+
+
+def test_noaa_data_is_accessible():
+    r = requests.get(data_url)
+    assert r.status_code == 200
+    assert len(r.json()) > 0
+
+def test_local_data():
+    df = pd.read_pickle(local_data)
+    assert len(df) > 0
